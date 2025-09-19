3238a9894e734f0badee414a639663dd0d5d70cd
Biresh paul <palbiresh375@gmail.com>
2025-09-19 15:40:20 +0600

    show countries in api and style added

--- src/component/countris/Countris.jsx
+++ src/component/countris/Countris.jsx
@@ -1,17 +1,22 @@
-import React, { use } from 'react';
-import Country from '../country/Country';
+import React, { use } from "react";
+import Country from "../country/Country";
+import "./countries.css";
 
-const Countris = ({countriesPromise}) => {
-    const countriesdata = use(countriesPromise)
-    const countries = countriesdata.countries
-    
+const Countris = ({ countriesPromise }) => {
+  const countriesdata = use(countriesPromise);
+  const countries = countriesdata.countries;
 
-    return (
-        <div>
-            <h1>countries {countries.length}</h1>
-            <Country countries={countries}></Country>
-        </div>
-    );
+  return (
+    <div>
+      <h1>countries {countries.length}</h1>
+      <div className="counrties">
+        {countries.map((country) => (
+        <Country key={country.cca3.cca3} country={country}></Country>
+      ))}
+
+      </div>
+    </div>
+  );
 };
 
 export default Countris;
--- src/component/country/Country.jsx
+++ src/component/country/Country.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React from "react";
+import "./country.css";
 
-const Country = ({countries}) => {
-    console.log(countries);
-    return (
-        <div>
-            <h1>from Country Component</h1>
-        </div>
-    );
+const Country = ({ country }) => {
+  console.log(country);
+  return (
+    <div className="country">
+      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+      <h1>Name: {country.name.common}</h1>
+      <h4>population : {country.population.population}</h4>
+      <h4>
+        Language :{" "}
+        {country.languages.languages?.eng ?? (
+          <span style={{ color: "red" }}>"not defiend"</span>
+        )}
+      </h4>
+      <h4>Capital : {country.capital.capital[0] ?? "not defiend"}</h4>
+      <p>
+        Area : {country.area.area}{" "}
+        {country.area.area > 300000 ? "Big country" : "samall country"}
+      </p>
+    </div>
+  );
 };
 
 export default Country;
